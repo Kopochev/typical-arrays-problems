fb1234f36e01b51a2a71abfae201d1234d09758c
Kopochev <copochev@yandex.ru>
2020-02-25 08:41:19 +0300

    feat: task is solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        let minValue = 0;
+        minValue = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        let maxValue = 0;
+        maxValue = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length > 0) {
+        let avgValue = 0;
+        let sum = 0
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        avgValue = sum / array.length;
+        return avgValue;
+    }
+    return 0;
 }
